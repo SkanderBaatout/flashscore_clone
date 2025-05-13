c9a6a18297faf2974230fe111f9ef54c13c463fc
SkanderBaatout <skanderbaatout2@gmail.com>
2025-05-13 10:57:52 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flashscore_clone/splasch_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flashscore Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/models/match.dart
@@ -0,0 +1,13 @@
+class Match {
+  final String teamA;
+  final String teamB;
+  final String score;
+  final String time;
+
+  Match({
+    required this.teamA,
+    required this.teamB,
+    required this.score,
+    required this.time,
+  });
+}
